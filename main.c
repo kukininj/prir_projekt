d2d4a95983298de3bb324c97d94d7508fa9992eb
Łukasz Niedzielski <luk20001122@gmail.com>
2024-10-06 18:13:14 +0000

    wyświetlanie postępu, czasu trwania, czasu przetwarzania partii, ilości hashy na sekunde

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "md5.c"
 
 #define MAX_PASSWORD_LENGTH 6
 
-const char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                         'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                         's', 't', 'u', 'v', 'v', 'w', 'x', 'y', 'z'};
+const size_t CHECK_COUNT = 1<<22;
+const char *alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 void print_hash(uint8_t *p) {
     for (unsigned int i = 0; i < 16; ++i) {
@@ -18,8 +18,29 @@ void print_hash(uint8_t *p) {
     printf("\n");
 }
 
+void print_progres(struct timeval *start, struct timeval *last_check,
+                   size_t checked_hashes) {
+    struct timeval now;
+
+    gettimeofday(&now, NULL);
+    double time_taken =
+        (now.tv_sec - start->tv_sec) + (now.tv_usec - start->tv_usec) / 1e6;
+
+    double time_taken_since_last_check =
+        (now.tv_sec - last_check->tv_sec) +
+        (now.tv_usec - last_check->tv_usec) / 1e6;
+
+    double speed = (1 / time_taken_since_last_check) * CHECK_COUNT;
+
+    gettimeofday(last_check, NULL);
+
+    printf("progress: %zu, total time: %f sec, batch time: %f sec, speed: %f "
+           "pass/sec.\n",
+           checked_hashes, time_taken, time_taken_since_last_check, speed);
+}
+
 int main(int argc, char *argv[]) {
-    char *actual_password = "asdfg";
+    char *actual_password = "zzzzzz";
     if (strlen(actual_password) > MAX_PASSWORD_LENGTH) {
         printf("Invalid password lenght\n");
         exit(0);
@@ -31,6 +52,10 @@ int main(int argc, char *argv[]) {
     size_t indices[MAX_PASSWORD_LENGTH] = {0};
     size_t alphabet_size = strlen(alphabet);
     size_t generated_hashes = 0;
+    struct timeval start, end, checkpoint;
+
+    gettimeofday(&start, NULL);
+    gettimeofday(&checkpoint, NULL);
 
     for (size_t length = 0; length <= MAX_PASSWORD_LENGTH; length++) {
         char buffer[MAX_PASSWORD_LENGTH] = {0};
@@ -52,19 +77,28 @@ int main(int argc, char *argv[]) {
                 pos--;
             }
 
-            // If we have incremented past the leftmost index, we are done
-            if (pos < 0) {
-                break;
-            }
             uint8_t result[16];
             md5String(buffer, length, result);
             generated_hashes++;
             if (memcmp(actual_password_hash, result, 16) == 0) {
+                gettimeofday(&end, NULL);
+                double time_taken = (end.tv_sec - start.tv_sec) +
+                                    (end.tv_usec - start.tv_usec) / 1e6;
                 print_hash(result);
-                printf("checked %zu hashes.\n", generated_hashes);
+                printf("checked %zu hashes, in %f seconds.\n", generated_hashes,
+                       time_taken);
                 return 0;
             }
             memset(buffer, 0, MAX_PASSWORD_LENGTH);
+
+            if ((generated_hashes & CHECK_COUNT - 1) == 0) {
+                print_progres(&start, &checkpoint, generated_hashes);
+            }
+
+            // If we have incremented past the leftmost index, we are done
+            if (pos < 0) {
+                break;
+            }
         }
     }
 }
