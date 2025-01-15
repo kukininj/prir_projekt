a67e262b42c6466bd2bafe6c41d9ef8e4848b7ee
lukasz niedzielski <luk20001122@gmail.com>
2025-01-15 23:43:42 +0100

    wyniki mpi_openmp

--- run_mpi_openmp.sh
+++ run_mpi_openmp.sh
@@ -4,10 +4,13 @@ make mpi_openmp
 
 echo "threads,time,hashes_checked"
 
-for n in 1 2 3 4 5 6 7
+for n in 2 4 6 8
 do
-  for i in {1..10}
+  for m in 2 4 6 8
   do
-    mpirun -n $n --oversubscribe ./md5 $n
+    for i in {1..10}
+    do
+      mpirun -n $n --oversubscribe ./md5 $m
+    done
   done
 done
